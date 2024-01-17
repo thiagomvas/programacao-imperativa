456b7991f95dff3b18e79aa4a68e7f80f0dd7207
Thiago <47324415+thiagomvas@users.noreply.github.com>
2024-01-17 14:45:49 -0300

    Create exercicio-8.c

---
+++ Lista 3/exercicio-8.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+
+int unidade(int num)
+{
+    return num % 10000 % 1000 % 100 % 10;
+}
+
+int dezena(int num)
+{
+    return num % 10000 % 1000 % 100 / 10;
+}
+
+int centena(int num)
+{
+    return num % 10000 % 1000 / 100;
+}
+
+int milhar(int num)
+{
+    return num % 10000 / 1000;
+}
+
+int dezenaMilhar(int num)
+{
+    return num / 10000;
+}
+
+int main() {
+    int num;
+    char escolha;
+    printf("Escolha um numero de 0 a 99999\n");
+    scanf("%d", &num);
+    printf("ESCOLHA UMA DAS OPÇÕES\nU - Unidade\nD - Dezena\nC - Centena\nM - Unidade de Milhar\nZ - Dezena de Milhar\n");
+    
+    scanf("%c", &escolha);
+    
+        switch(escolha)
+        {
+            case 'U':
+                printf("%d", unidade(num));
+                break;
+            case 'D':
+                printf("%d", dezena(num));
+                break;
+            case 'C':
+                printf("%d", centena(num));
+                break;
+            case 'M':
+                printf("%d", milhar(num));
+                break;
+            case 'Z':
+                printf("%d", dezenaMilhar(num));
+                break;
+            default:
+                printf("ESCOLHA INVALIDA");
+                break;
+        
+    }
+    
+    
+}
