188843ab09de5414be188e818dd5a69690618ed7
Thiago <47324415+thiagomvas@users.noreply.github.com>
2024-01-24 12:12:49 -0300

    Update exercicio-7.c

--- Lista 2/exercicio-7.c
+++ Lista 2/exercicio-7.c
@@ -1,36 +1,34 @@
 /*
- * 7.Escrever um programa em C sobre investimento. Deve ser efetuada a
-leitura do valor inicial investido, do valor investido mensalmente e do tempo
-de investimento (desconsiderar juros) e retornar o valor final (montante) e o
-valor do rendimento (montante – valor inicial investido). Deve ser aplicado
-um módulo(função) com dois parâmetros de saída.
+ * 8.Escrever um programa em C para ler um valor V em reais, taxa de
+câmbio fixa de dólar e taxa de câmbio fixa de euro e exibir em tela os
+correspondentes a V em dólar e em euro. Para tanto, criar função F em que
+dados valor e taxa de câmbio, retorna o valor convertido. Criar módulo com
+aplicação de F, parâmetros de entrada e dois parâmetros de saída: valor em
+dólar e valor em euro.
  */
+
 #include <stdio.h>
 
-void calcularInvestimento(double valorInicial, double valorMensal, int tempo, double *valorFinal, double *rendimento)
+void calcularCambio(float valor, float cambioDolar, float cambioEuro, float *dolar, float *euro)
 {
-    *valorFinal = valorInicial + valorMensal * tempo;
-    *rendimento = *valorFinal - valorInicial;
+    *dolar = valor / cambioDolar;
+    *euro = valor / cambioEuro;
 }
-
 
 void main()
 {
-    double valorInicial, valorMensal, valorFinal, rendimento;
-    int tempo;
+    float cambioDolar, cambioEuro, valor, dolar, euro;
+    printf("Quantos reais voce tem?\n");
+    scanf("%f", &valor);
+    printf("Qual o valor do cambio do dolar para real? Isto e, quantos reais valem 1 dolar americano?\n");
+    scanf("%f", &cambioDolar);
+    printf("Qual o valor do cambio do euro para real? Isto e, quantos reais valem 1 euro?\n");
+    scanf("%f", &cambioEuro);
 
-    printf("Quanto foi investido inicialmente?\n");
-    scanf("%lf", &valorInicial);
+    calcularCambio(valor, cambioDolar, cambioEuro, &dolar, &euro);
 
-    printf("Quanto vai ser investido mensalmente?\n");
-    scanf("%lf", &valorMensal);
-
-    printf("Quantos meses passara investindo?\n");
-    scanf("%d", &tempo);
-
-    calcularInvestimento(valorInicial, valorMensal, tempo, &valorFinal, &rendimento);
-
-    printf("Valor final investido: %.2lf\n"
-           "Rendimento: %0.2lf",
-           valorFinal, rendimento);
+    printf("Valor em real: %0.2f BRL\n"
+           "Valor em dolar: %0.2f USD\n"
+           "Valor em euro: %0.2f EUR\n",
+           valor, dolar, euro);
 }
