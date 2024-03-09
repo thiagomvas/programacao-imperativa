5a88b4376fedace7e2f753a9f7a1143cdbc19fc8
Thiago <47324415+thiagomvas@users.noreply.github.com>
2024-03-09 12:35:34 -0300

    Update exercicio-2.c

--- Lista 6/exercicio-2.c
+++ Lista 6/exercicio-2.c
@@ -4,9 +4,22 @@
 
 #define MAX_NOME 50
 
-void formatarNome(char nome[MAX_NOME]);
+void formatarNome(char nome[MAX_NOME]) 
+{
+    nome[0] = toupper(nome[0]);
 
-int main() {
+    for (int i = 1; nome[i] != '\0'; i++) 
+    {
+        if (nome[i - 1] == ' ') // Não entendi se queria dizer apenas a primeira letra do nome como um todo ou se era a primeira letra de cada sobrenome
+            nome[i] = toupper(nome[i]);
+        else 
+            nome[i] = tolower(nome[i]);
+        
+    }
+}
+
+int main() 
+{
     char nome[MAX_NOME];
 
     printf("Digite um nome: ");
@@ -19,11 +32,4 @@ int main() {
     printf("Nome formatado: %s\n", nome);
 
     return 0;
-}
-
-void formatarNome(char nome[MAX_NOME]) {
-    nome[0] = toupper(nome[0]);
-    for (int i = 1; nome[i] != '\0'; i++) {
-        nome[i] = tolower(nome[i]);
-    }
 }
