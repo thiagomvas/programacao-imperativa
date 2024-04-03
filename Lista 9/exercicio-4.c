45b013cc88743c06373956f9c90e455557461d15
Thiago <47324415+thiagomvas@users.noreply.github.com>
2024-04-02 22:34:43 -0300

    Create exercicio-4.c

---
+++ Lista 9/exercicio-4.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TOTAL_PALAVRAS 25
+#define TAMANHO_PALAVRA 50
+
+// Função para ordenar um array de strings usando o algoritmo de seleção
+void selectionSort(char arr[][TAMANHO_PALAVRA], int n) {
+    int i, j, min_idx;
+    char temp[TAMANHO_PALAVRA];
+
+    // Itera sobre o array até o penúltimo elemento
+    for (i = 0; i < n-1; i++) {
+        min_idx = i;
+        // Encontra o índice do menor elemento no restante do array
+        for (j = i+1; j < n; j++) {
+            if (strcmp(arr[j], arr[min_idx]) < 0)
+                min_idx = j;
+        }
+        // Se o índice do menor elemento for diferente do índice atual, realiza a troca
+        if (min_idx != i) {
+            strcpy(temp, arr[min_idx]);
+            strcpy(arr[min_idx], arr[i]);
+            strcpy(arr[i], temp);
+        }
+    }
+}
+
+void printArray(char arr[][TAMANHO_PALAVRA], int tamanho) {
+    printf("[");
+    for (int i = 0; i < tamanho; i++)
+    {
+        printf("%s", arr[i]);
+        if(i < tamanho - 1)
+            printf(", ");
+        
+    }
+    printf("]");
+}
+
+int main() {
+    int n, i;
+
+    char palavras[][TAMANHO_PALAVRA] = { "banana", "maçã", "laranja", "uva", "abacaxi", "melancia", "mamão", "morango", "tapioca", "limão", "pera", "kiwi", "ameixa", "goiaba", "cereja", "abacate", "caqui", "pitaya", "framboesa", "nectarina", "tangerina", "manga", "caju", "jabuticaba", "figo" };
+    
+    printf("\nArray de palavras não ordenado: \n");
+    printArray(palavras, TOTAL_PALAVRAS);
+
+    selectionSort(palavras, TOTAL_PALAVRAS);
+
+    printf("\nArray de palavras ordenado: \n");
+    printArray(palavras, TOTAL_PALAVRAS);
+    
+    return 0;
+}
